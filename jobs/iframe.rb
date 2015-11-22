400568112c414723f17f04442c00c5169b2ef10e
hunii <jjoung@hotmail.co.nz>
2015-11-23 12:33:52 +1300

    d

--- jobs/iframe.rb
+++ jobs/iframe.rb
@@ -2,7 +2,7 @@
 
 
 SCHEDULER.every '20m', :first_in => 0 do |job|
-    url = "https://www.google.com"
+    url = "http://www.youtube.com/embed/XGSy3_Czz8k"
     puts url
     send_event('myiframe', src: url)
 end
