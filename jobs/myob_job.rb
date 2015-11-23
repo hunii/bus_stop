db5fbb35b6b5fda561c6278932c197bf2e0793c9
hunii <jjoung@hotmail.co.nz>
2015-11-24 12:17:10 +1300

    d

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -1,43 +1,13 @@
-# require 'net/http'
-# require 'open-uri'
-# require 'nokogiri'
-
-# url = "http://fsr.cvmail.com/au/myob/main.cfm"
-# data = Nokogiri::HTML(open(url))
+require 'json'
 
 
-# continued = 0
-# i = 0
-# p = 0
+data = "[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\", \"lastName\":\"Smith\"},{\"firstName\":\"Peter\",\"lastName\": \"Jones\"}
+\]"
+
+puts data
 
 
-# SCHEDULER.every '1h', :first_in => 0 do
-# listing = ""
+jjson = JSON.parse(data)
 
-
-# numOfJobs = data.css('.textEmphasisStyle').text.strip
-# number = (numOfJobs.length%33)/2
-# if number == 11
-#   number -=1
-# end
-# if number == 13
-#   number -=2
-# end
-# if number == 15
-#   number -=3
-# end
-
-# until continued == number do
-#   listing += data.css('.textEmphasisStyle')[p].text.strip+" "
-#   listing += data.css('.jbTableTextStyle')[i].text.strip+"\n Closing on: "
-#   listing += data.css('.jbTableTextStyle')[i+=1].text.strip+" Loc: "
-#   listing += data.css('.jbTableTextStyle')[i+=1].text.strip+" \n\n"
-#   p += 2
-#   i+=1
-#   continued += 1
-# end
-
-
-#   send_event('myob_list', { quote: listing })
-
-# end
+send_event('testtest',   comments: jjson )
+puts jjson
