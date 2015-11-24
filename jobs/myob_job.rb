8525b8d7c827c46daf4354f45ac97918bfeb8eab
hunii <jjoung@hotmail.co.nz>
2015-11-25 09:50:31 +1300

    d

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -2,7 +2,7 @@ require 'json'
 
 
 data = [{firstName:"John", lastName:"Doe"},{firstName:"Anna", lastName:"Smith"},{firstName:"Peter",lastName: "Jones"}]
-
+datajson = data.to_json
 
 
 items = [{label: "l1", value: "v1"},{label: "l2", value: "v2"},{label: "l3", value: "v3"}]
@@ -13,7 +13,7 @@ json_formatted_items = items.to_json
 
 SCHEDULER.every '3s', :first_in => 0 do |job|
   send_event('testtest', {items: json_formatted_items })
-  send_event('testtest1', {items: data })
+  send_event('testtest1', {items: datajson })
 end
 
 
