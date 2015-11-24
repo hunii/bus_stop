435d0979d42522221433f7562c2ac58a72855355
hunii <jjoung@hotmail.co.nz>
2015-11-25 11:36:03 +1300

    d

--- jobs/test.rb
+++ jobs/test.rb
@@ -6,13 +6,15 @@ require 'json'
 openafile = File.open("trytextfile.txt", "r+")
 readafile = openafile.read
 
+openafile1 = File.open("/trytextfile.txt", "r+")
+readafile1 = openafile1.read
 
 
 
 SCHEDULER.every '1s', :first_in => 0 do |job|
 
   send_event('textfile', {items: readafile })
- 
+  send_event('textfile1', {items: readafile1 })
 
 
 end
