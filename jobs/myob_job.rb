6c54ca91dfdc308840c73262289b85f9042c749f
hunii <jjoung@hotmail.co.nz>
2015-11-25 09:28:13 +1300

    d

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -6,13 +6,23 @@ data = "[{\"firstName\":\"John\", \"lastName\":\"Doe\"},{\"firstName\":\"Anna\",
 
 
 
+items = [{label: "l1", value: "v1"},{label: "l2", value: "v2"},{label: "l3", value: "v3"}]
+json_formatted_items = items.to_json
+# => "[{\"label\":\"l1\",\"value\":\"v1\"},{\"label\":\"l2\",\"value\":\"v2\"},{\"label\":\"l3\",\"value\":\"v3\"}]"
+
+
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  send_event('testtest', {items: json_formatted_items })
+end
+
 
 
 SCHEDULER.every '3s', :first_in => 0 do
 
 
-  send_event('testtest',  { comments: rand(100) } )
-  send_event('testtest1',  { comments: rand(100) } )
+  send_event('testtest4',  { items: rand(100) } )
+  send_event('testtest14',  { items: rand(100) } )
 
 end
 
