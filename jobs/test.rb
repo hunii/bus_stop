5509554e3254378036c3c221ac45625eb4f7cfc0
hunii <jjoung@hotmail.co.nz>
2015-11-25 11:56:10 +1300

    d

--- jobs/test.rb
+++ jobs/test.rb
@@ -4,14 +4,15 @@ require 'json'
 
 
 openafile = File.open("trytextfile.txt", "r+")
-readafile = openafile.read
+readafile123 = openafile.read
+readafile = JSON.parse(readafile123)
 
 config_file = File.dirname(File.expand_path(__FILE__)) + '/../trytextfile.txt'
 config = File.open(config_file)
 readafile1 = config.read
 
 
-SCHEDULER.every '1s', :first_in => 0 do |job|
+SCHEDULER.every '1m', :first_in => 0 do |job|
 
   send_event('textfile', {items: readafile })
   send_event('textfile1', {items: readafile1 })
