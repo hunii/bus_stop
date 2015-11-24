be23377df91eeb687306a3299a35c992452c3c76
hunii <jjoung@hotmail.co.nz>
2015-11-24 16:19:44 +1300

    d

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -22,13 +22,21 @@ require 'json'
 # 		:firstName =>
 # 	}
 
-url = File.open("/jobs/test1.txt", "r")
-contents = url.read
-puts contents
+current_valuation = 0
+current_karma = 0
 
 
 SCHEDULER.every '5s', :first_in => 0 do
+  last_valuation = current_valuation
+  last_karma     = current_karma
+  current_valuation = rand(100)
+  current_karma     = rand(200000)
 
-	send_event('testtest',  {comments: contents} )
-	send_event('testtest1',  comments: contents )
+  send_event('testtest',  { value: contents } )
+  
+
+  send_event('valuation', { current: current_valuation, last: last_valuation })
+  send_event('karma', { current: current_karma, last: last_karma })
+  send_event('synergy',   { value: rand(100) })
 end
+
