dafd9e2035ef67e0d7c58fc09399d1bbe764f217
hunii <jjoung@hotmail.co.nz>
2015-11-25 10:59:54 +1300

    d

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -2,7 +2,7 @@ require 'json'
 
 usingList = [{:label=>"Count", :value=>10}, { :label=>"Sort", :value=>30}]
 
-usingList1 = {:label=>"Count", :value=>10}
+usingList1 = [{:quote=>"Count"}]
 
 
 data = [{firstName:"John", lastName:"Doe"},{firstName:"Anna", lastName:"Smith"},{firstName:"Peter",lastName: "Jones"}]
@@ -21,7 +21,7 @@ json_formatted_items = items.to_json
 SCHEDULER.every '1m', :first_in => 0 do |job|
 
   send_event('myob_list', {items: usingList })
-  send_event('testtest1', {items: usingList1 })
+  send_event('testtest1', {comments: usingList1 })
 
 
 end
