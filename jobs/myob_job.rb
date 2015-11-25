11c2363dda130dd9fa337fb1f79b123b6737fa08
hunii <jjoung@hotmail.co.nz>
2015-11-25 15:18:50 +1300

    d

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -1,9 +1,14 @@
 require 'json'
 
-usingList = [{:label=>"Deveoper", :value=>"30/20/2015"}, { :label=>"Sort", :value=>30}]
+usingList = [{:label=>"Developer", :value=>"30/10/2015"}, { :label=>"Tester", :value=>"20/12/2015"}]
+
+
+jasonfile ="{\"label\":\"Alastair Cook\"}"
+
+parsedfile = JSON.parse(jasonfile)
+
 
 usingComment = [{:name=>"MYOB", :body=>"Using Comment work!!"}]
-
 
 
 
@@ -12,6 +17,9 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 
   send_event('myob_list', {items: usingList })
   send_event('testtest1', {comments: usingComment })
+
+	send_event('test1', {items: parsedfile })
+  send_event('test2', {items: usingList })
 
 
 end
