6fe760a1c3c5d6f90ee59a1dbe6097ad77c0987b
hunii <jjoung@hotmail.co.nz>
2015-11-26 12:38:08 +1300

    g

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -12,13 +12,16 @@ config = File.read(config_file)
 configjson = [JSON.parse(config)]
 
 
-textstring = "Jamesstring"
+iterate = 0
+config_file = File.dirname(File.expand_path(__FILE__)) + '/../joblist.txt'
+config = File.read(config_file)
+list = config.split("//")
 
+textstring = "Jamesstring"
 textstring2 = ["123234345", "0000", "333", "4444"]
 
 
 
-n = 0
 
 # config_file1 = File.dirname(File.expand_path(__FILE__)) + '/../trytextfile1.txt'
 # config1 = File.read(config_file1)
@@ -38,15 +41,13 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
   send_event('myob_list', {items: usingList })
   send_event('testtest1', {comments: usingComment })
   
- 
-  if n != 2
-  	data = [{:label=>textstring, :value=> textstring2[n]}]
-  	n += 1
-  else
-  	data = [{:label=>textstring, :value=> textstring2[n]}]
-  	n = 0
-  end
 
+  if iterate != 2
+  	iterate += 1
+  else
+  	iterate = 0
+  end
+   data = [{:label=>textstring, :value=> list[n]}]
    send_event('test1', {items: data })
 
 
