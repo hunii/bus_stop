03cf3ac2940756be45a071109b5bd0417dd4f903
hunii <jjoung@hotmail.co.nz>
2015-11-25 13:08:32 +1300

    d

--- jobs/test.rb
+++ jobs/test.rb
@@ -3,22 +3,27 @@ require 'json'
 
 
 
-openafile = File.open("./trytextfile.txt", "r+")
-readafile = openafile.read
+openafile = File.open('trytextfile.txt')
+readafile = JSON.parse(openafile)
 
-select = readafile["body"]
+newArray = []
+key = readafile.keys
+key.each do |kkey|
+	array_value = readafile[kkey]
+	array = Hash.new("label", kkey, "value", array_value)
+	newArray.push(array)
+end
 
-
-config_file = File.dirname(File.expand_path(__FILE__)) + '/../trytextfile.txt'
-config = File.open(config_file)
-readafile1 = config.read
+# config_file = File.dirname(File.expand_path(__FILE__)) + '/../trytextfile.txt'
+# config = File.open(config_file)
+# readafile1 = config.read
 
 select1 = readafile1["body"]
 
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
 
-  send_event('textfile', {items: select })
+  send_event('textfile', {items: newArray })
   send_event('textfile1', {items: select1 })
 
 
