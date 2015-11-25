a3b60667b31ba07c119ae72fbbf380f0b997b7ac
hunii <jjoung@hotmail.co.nz>
2015-11-25 16:28:33 +1300

    g

--- jobs/myob_job.rb
+++ jobs/myob_job.rb
@@ -27,7 +27,7 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
   send_event('testtest1', {comments: usingComment })
 
 	send_event('test1', {items: configjson })
-  send_event('test2', {items: usingList1 })
+  send_event('test2', {items: parsedfile })
 
 
 end
